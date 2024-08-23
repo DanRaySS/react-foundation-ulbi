c4a0d87819f95292434d3a7bfb750d6cef502018
Daniil <113042616+DanRaySS@users.noreply.github.com>
2024-08-23 16:31:13 +0500

    controlled components / uncontrolled components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React, { useState } from "react";
-import Counter from "./components/Counter";
-import ClassCounter from "./components/ClassCounter";
+import React, { useRef, useState } from "react";
 import './styles/App.css';
-import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
   const [posts, setPosts] = useState([
     { id: 1, title: 'JS', body: 'Description' },
-    { id: 2, title: 'JS', body: 'Description' },
-    { id: 3, title: 'JS', body: 'Description' },
+    { id: 2, title: 'JS 2', body: 'Description' },
+    { id: 3, title: 'JS 3', body: 'Description' },
   ]);
+  const [title, setTitle] = useState('');
+  const bodyInputRef = useRef();
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log(title);
+    console.log(bodyInputRef.current.value);
+  };
+
   return (
     <div className="App">
-      <h1 style={{ textAlign: 'center' }}>Список постов</h1>
-      {posts.map(post =>
-        <PostItem post={post} key={post.id} />
-      )}
+      <form>
+        {/* Управляемый компонент */}
+        <MyInput value={title} onChange={e => setTitle(e.target.value)} type="text" placeholder="Название поста" />
+        {/* Неуправляемый компонент/Неконтролируемый компонент */}
+        <MyInput ref={bodyInputRef} type="text" placeholder="Описание поста" />
+        <MyButton onClick={addNewPost} >Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title="Посты про JS" />
     </div>
   );
 }
